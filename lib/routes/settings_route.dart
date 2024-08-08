066d6c44f62c231ae4bba71c7db08b33cc9b9d39
LJ5O <75009579+LJ5O@users.noreply.github.com>
2024-08-08 23:37:49 +0200

    Added Tabs in settings
    Easier to find something in the UI

--- lib/routes/settings_route.dart
+++ lib/routes/settings_route.dart
@@ -1,12 +1,59 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingsRoute extends StatelessWidget{
+class SettingsRoute extends StatefulWidget{
   const SettingsRoute({super.key});
 
   @override
+  State<SettingsRoute> createState() => _SettingsRouteState();
+
+}
+
+class _SettingsRouteState extends State<SettingsRoute> {
+
+  @override
   Widget build(BuildContext context) {
-    return Text("TEST");
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Paramètres"),
+            backgroundColor: Colors.blueGrey,
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "Fruits", icon: FaIcon(FontAwesomeIcons.appleWhole)),
+                Tab(text: "Légumes", icon: FaIcon(FontAwesomeIcons.carrot)),
+                Tab(text: "Autres", icon: FaIcon(FontAwesomeIcons.warehouse))
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              const Text("Fruits"),
+              const Text("Légumes"),
+              ReorderableListView(
+                onReorder: (int oldIndex, int newIndex){
+                  setState(() {
+                    //TODO
+                  });
+                },
+                children: <Widget>[
+                  for(int index = 0; index<5; index ++)
+                    ListTile(
+                        key: Key("$index"),
+                        title: Text("$index"),
+                      trailing: ReorderableDragStartListener(
+                        index: index,
+                        child: const Icon(Icons.drag_handle),
+                      ),
+                    )
+                  ],
+              )
+            ],
+          )
+      ),
+    );
   }
 
 }
