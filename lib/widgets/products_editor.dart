9a96367a92259b23d6b9fc7360c76b464387e244
LJ5O <75009579+LJ5O@users.noreply.github.com>
2024-08-09 13:58:51 +0200

    We can now manage products lists
    We can now add, order and delete products from lists.

--- lib/widgets/products_editor.dart
+++ lib/widgets/products_editor.dart
@@ -16,6 +16,7 @@ class ProductsEditor extends StatefulWidget {
 class _ProductsEditorState extends State<ProductsEditor>{
 
   StorageService storage = StorageService();
+  List<String> products = [];
 
   void _updateProductList(int oldIndex, int newIndex, List<String> products) async {
     setState(() {
@@ -26,8 +27,62 @@ class _ProductsEditorState extends State<ProductsEditor>{
       products.insert(newIndex, item);
     });
 
-    // Appel asynchrone en dehors de setState
+    // Saving the new list behind the scenes
     await storage.setStringList(widget.productType, products);
+  }
+
+  void _addProduct(String productName, List<String> products) async {
+    setState(() {
+      products.insert(0, productName);
+    });
+    // Saving the new list behind the scenes
+    await storage.setStringList(widget.productType, products);
+  }
+
+  void _removeProduct(int index, List<String> products) async {
+    setState(() {
+      products.removeAt(index);
+    });
+    // Saving the new list behind the scenes
+    await storage.setStringList(widget.productType, products);
+  }
+
+  void _showAddProductDialog(BuildContext context) {
+    String productName = '';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Ajouter un produit"),
+          content: TextField(
+            onChanged: (value) {
+              productName = value.trim();
+            },
+            decoration: const InputDecoration(
+              hintText: "Nom du produit",
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close, do nothing
+              },
+              child: const Text("Annuler"),
+            ),
+            TextButton(
+              onPressed: () {
+                if(productName!=""){
+                  _addProduct(productName, products);
+                }
+                Navigator.of(context).pop(); // Fermer la popup
+              },
+              child: const Text("Ajouter"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -37,37 +92,61 @@ class _ProductsEditorState extends State<ProductsEditor>{
       throw AssertionError('Product Type should be "vegetables", "fruits" or "others" !');
     }
 
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.secondary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.secondary.withOpacity(0.15);
+
     return FutureBuilder(
       future: storage.getStringList(widget.productType),
       builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot){
-        List<String> products = ["Cerise", "Fraise", "Framboise", "Pomme"] ?? snapshot.data ?? [];
+        products = snapshot.data ?? [];
         if(products.isEmpty){//No products
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text("Aucun élément n'est dans cette liste."),
-                ElevatedButton(onPressed: null, child: Text("Ajouter"))
+              children: [
+                const Text("Aucun élément n'est dans cette liste."),
+                ElevatedButton(
+                    onPressed: (){_showAddProductDialog(context);},
+                    child: const Text("Ajouter")
+                )
               ],
             )
           );
         }else{
-          return ReorderableListView(
-            onReorder: (int oldIndex, int newIndex){
-              _updateProductList(oldIndex, newIndex, products);
-            },
-            children: <Widget>[
-              for(int index = 0; index<products.length; index ++)
-                ListTile(
-                  key: Key(products[index]),
-                  title: Text(products[index]),
-                  tileColor: index%2==0 ? Colors.white : Colors.white10,
-                  trailing: ReorderableDragStartListener(
-                    index: index,
-                    child: const Icon(Icons.drag_handle),
-                  ),
-                )
+          return Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _showAddProductDialog(context);
+                },
+                child: const Text("Ajouter un produit"),
+              ),
+              Expanded(
+                child: ReorderableListView(
+                  onReorder: (int oldIndex, int newIndex) {
+                    _updateProductList(oldIndex, newIndex, products);
+                  },
+                  children: <Widget>[
+                    for (int index = 0; index < products.length; index++)
+                      ListTile(
+                        key: Key(products[index]),
+                        title: Text(products[index]),
+                        tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                        trailing: ReorderableDragStartListener(
+                          index: index,
+                          child: const Icon(Icons.drag_handle),
+                        ),
+                        leading: ElevatedButton(
+                          style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                          onPressed: (){_removeProduct(index, products);},
+                          child: const Icon(Icons.delete_forever)
+                        ),
+                      ),
+                  ],
+                ),
+              ),
             ],
           );
         }
