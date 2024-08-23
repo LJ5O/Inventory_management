eed3221e980a3ad50241abc9774446ed87f7c004
LJ5O <75009579+LJ5O@users.noreply.github.com>
2024-08-23 19:39:22 +0200

    Removed useless imports

--- lib/routes/market_route.dart
+++ lib/routes/market_route.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:inventaire/widgets/products_editor.dart';
 import 'package:inventaire/widgets/products_reset.dart';
 import 'package:inventaire/widgets/products_sales.dart';
-import 'package:inventaire/widgets/products_viewer.dart';
 
 class MarketRoute extends StatefulWidget{
   const MarketRoute({super.key});
--- lib/widgets/products_reset.dart
+++ lib/widgets/products_reset.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../providers/shared_provider.dart';
