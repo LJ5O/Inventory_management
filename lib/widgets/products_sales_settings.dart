383859a42d386ce76233138c149539bb101794b1
LJ5O <75009579+LJ5O@users.noreply.github.com>
2024-08-28 02:09:26 +0200

    We can now properly define the low availability threshold

--- lib/widgets/products_sales_settings.dart
+++ lib/widgets/products_sales_settings.dart
@@ -25,43 +25,74 @@ class ProductsSalesSettings extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     StorageService storage = StorageService();
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(right: 15, left: 15),
-          child: TextField(
-              decoration: const InputDecoration(
-                  labelText: "Seuil d'alerte de faible stock ( Stock - vente )",
-                  labelStyle: TextStyle(letterSpacing: 0, wordSpacing: -1)
+
+    return FutureBuilder<int>(
+      future: storage.getInt('low_stock_threshold'),  // Getting default value for low availability warning
+      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();  // Afficher un loader pendant le chargement
+        } else {
+          int initialValue = snapshot.data ?? 0;
+          TextEditingController textController = TextEditingController(text: initialValue.toString());
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 15, left: 15),
+                child: TextField(
+                  controller: textController,
+                  decoration: const InputDecoration(
+                    labelText: "Seuil d'alerte de faible stock ( Stock - vente )",
+                    labelStyle: TextStyle(letterSpacing: 0, wordSpacing: -1),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  onChanged: (value) {
+                    if (value == '') value = '0';
+                    storage.setInt('low_stock_threshold', int.parse(value));
+                  },
+                ),
               ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              onChanged: (value){
-                if(value == '') value = '0';
-                storage.setInt('low_stock_threshold', int.parse(value));
-              }
-          )
-        ),
-        const Padding(padding: EdgeInsets.all(40.0)),
-        const Text("Ce bouton permet de remettre tous les compteurs du marché à 0, et retirera les valeurs de ce qu'il reste en stock.", textAlign: TextAlign.center),
-        ElevatedButton(
-            onPressed: (){resetSalesProducts(true);Navigator.pop(context);},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-            child: const Text("Remettre les ventes à zéro, et retirer cela du stock", style: TextStyle(color: Colors.white))
-        ),
-        const Padding(padding: EdgeInsets.all(20.0)),
-        const Text("Ce bouton va remettre tous les compteurs du marché à 0, SANS retirer les valeurs du stock.", textAlign: TextAlign.center),
-        ElevatedButton(
-            onPressed: (){resetSalesProducts(false);Navigator.pop(context);},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-            child: const Text("Remettre les ventes à zéro, sans retirer du stock", style: TextStyle(color: Colors.white))
-        )
-      ],
+              const Padding(padding: EdgeInsets.all(40.0)),
+              const Text(
+                "Ce bouton permet de remettre tous les compteurs du marché à 0, et retirera les valeurs de ce qu'il reste en stock.",
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  resetSalesProducts(true);
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                child: const Text(
+                  "Remettre les ventes à zéro, et retirer cela du stock",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(20.0)),
+              const Text(
+                "Ce bouton va remettre tous les compteurs du marché à 0, SANS retirer les valeurs du stock.",
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  resetSalesProducts(false);
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                child: const Text(
+                  "Remettre les ventes à zéro, sans retirer du stock",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          );
+        }
+      },
     );
   }
-  
 }
